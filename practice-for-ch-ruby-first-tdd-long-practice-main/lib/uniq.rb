80f7d7ac42e76704f7eab5e50bf9a3f0c3c9a5af
vincentpham123 <vincentpham32@gmail.com>
2023-04-21 14:57:33 -0700

    finished uniq

---
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/uniq.rb
@@ -0,0 +1,5 @@
+
+def uniq(array)
+    raise 'Not an array' if !array.is_a?(Array)
+    array.uniq
+end
---
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/uniq_spec.rb
@@ -0,0 +1,24 @@
+require 'uniq'
+
+
+describe '#uniq' do
+    let(:test1) {[1,2,3]}
+    let (:test2){[1, 2, 1, 3, 3]}
+    context 'when not an array is given' do 
+        it 'returns an error' do 
+            expect{uniq('test')}.to raise_error('Not an array')
+        end
+    end
+    context 'when given an array with no repeats' do
+        it 'returns a different array' do 
+            expect(uniq(test1)).not_to be(test1)
+        end
+    end
+    
+    it 'returns unique values' do
+        expect(uniq(test2)).to eq(test1)
+    end
+
+
+
+    end
